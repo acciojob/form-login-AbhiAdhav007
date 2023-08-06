fa295567dafb9b2ae2017b9949b1aa5f0f2b1498
AbhiAdhav007 <120732315+AbhiAdhav007@users.noreply.github.com>
2023-08-06 15:46:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const firstName = document.getElementbyId('first').value ;
+const lastName = document.getElementbyId('last').value;
 function getFormvalue() {
     //Write your code here
-
+		alert(firstName+ " " + lastName.value);
+	
 }
